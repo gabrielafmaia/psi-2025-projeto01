5e69755244b8646b4c409bbc46245581d5d8dc5e
Gabriela Maia <gabrielamaia.f12@gmail.com>
2025-07-04 17:50:25 -0300

    atualizando app

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'app/index.html')
+
+def elenco(request):
+    return render(request, 'app/elenco.html')
+
+def sobre(request):
+    return render(request, 'app/sobre.html')
