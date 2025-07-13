0547614157793e0a7b18a12c8b81684c223d17ba
Gabriela Maia <gabrielamaia.f12@gmail.com>
2025-07-13 02:34:19 -0300

    atualizando templates e views

--- app/views.py
+++ app/views.py
@@ -1,71 +1,102 @@
 from django.shortcuts import render
 
-introducao = [
-    {
-        "titulo": "História",
-        "conteudo": "Em 22 de setembro de 1994, a NBC exibia o primeiro episódio de Friends, série criada por David Crane e Marta Kauffman que se tornou um fenômeno mundial. A trama sobre seis amigos na casa dos 20 anos que lutam para encontrar estabilidade emsua vida ganhou o mundo, e transformou o sexteto formado por Jennifer Aniston, Courteney Cox, Lisa Kudrow, Matt LeBlanc, Matthew Perry e David Schwimmer ícones da TV.",
-        "imagem": "app/img/friends-frame"
-    }
-]
+introducao = {
+    "titulo": "História",
+    "subtitulo": "A sitcom de sucesso",
+    "conteudo": "Em 22 de setembro de 1994, a NBC exibia o primeiro episódio de Friends, série criada por David Crane e Marta Kauffman que se tornou um fenômeno mundial. A trama sobre seis amigos que lutam para encontrar estabilidade em sua vida ganhou o mundo, e transformou o sexteto formado por Jennifer Aniston, Courteney Cox, Lisa Kudrow, Matt LeBlanc, Matthew Perry e David Schwimmer ícones da TV. O impacto cultural conquistado por Friends fez da série uma das mais amadas da história da televisão. Seus personagens são lembrados até hoje pelos fãs, enquanto seus episódios ficam em constante reexibição em centenas de países, sendo disponibilizados em plataformas de streaming e ainda figurando entre os programas mais assistidos, décadas após o seu fim.",
+    "primeira_foto": "app/img/photo-one.jpg",
+    "segunda_foto": "app/img/photo-two.jpg",
+    "terceira_foto": "app/img/photo-three.jpg",
+}
 
 atores = [
     {
-    "nome": "Jennifer Aniston",
-    "idade": 56,
-    "id": 1,
-    "local_nasc": "Sherman Oaks, Los Angeles, Califórnia, EUA",
-    "imagem": "app/img/cast-rachel.webp"
+        "nome": "Jennifer Aniston",
+        "personagem": "Rachel Green",
+        "idade": 56,
+        "id": 1,
+        "local_nasc": "Califórnia, EUA",
+        "imagem": "app/img/cast-rachel.webp",
     },
 
     {
-    "nome": "David Schwimmer",
-    "idade": 58,
-    "id": 2,
-    "local_nasc": "Flushing, Queens, Nova York, EUA",
+        "nome": "Courteney Cox",
+        "personagem": "Monica Geller",
+        "idade": 61,
+        "id": 2,
+        "local_nasc": "Alabama, EUA",
+        "imagem": "app/img/cast-monica.webp",
     },
 
     {
-    "nome": "Courteney Cox",
-    "idade": 61,
-    "id": 3,
-    "local_nasc": " Birmingham, Alabama, EUA",
+        "nome": "Lisa Kudrow",
+        "personagem": "Phoebe Buffay",
+        "idade": 62,
+        "id": 3,
+        "local_nasc": "Califórnia, EUA",
+        "imagem": "app/img/cast-phoebe.webp",
     },
 
     {
-    "nome": "Lisa Kudrow",
-    "idade": 62,
-    "id": 4,
-    "local_nasc": "Encino, Los Angeles, EUA",
+        "nome": "David Schwimmer",
+        "personagem": "Ross Geller",
+        "idade": 58,
+        "id": 4,
+        "local_nasc": "Nova York, EUA",
+        "imagem": "app/img/cast-ross.webp",
     },
 
     {
-    "nome": "Matt LeBlanc",
-    "idade": 57,
-    "id": 5,
-    "local_nasc": "Newton, Massachusetts, EUA",
+        "nome": "Matt LeBlanc",
+        "personagem": "Joey Tribbiani",
+        "idade": 57,
+        "id": 5,
+        "local_nasc": "Massachusetts, EUA",
+        "imagem": "app/img/cast-joey.webp",
     },
 
     {
-    "nome": "Matthew Perry",
-    "idade": 54,
-    "id": 6,
-    "local_nasc": "Williamstown, Massachusetts, EUA",
+        "nome": "Matthew Perry",
+        "personagem": "Chandler Bing",
+        "idade": 54,
+        "id": 6,
+        "local_nasc": "Massachusetts, EUA",
+        "imagem": "app/img/cast-chandler.webp",
+    }
+]
+
+sobre = [
+    {
+        "titulo": "Sobre o site",
+        "conteudo": "<p class='w3-justify'>Este site foi desenvolvido como parte do nosso primeiro projeto na disciplina de Programação de Sistemas para Internet, com objetivo de colocar em prática os conhecimentos adquiridos em sala de aula sobre desenvolvimento de sites dinâmicos. Para isso, utilizamos o framework Django.</p>",
+    },
+
+    {
+        "titulo": "Autores",
+        "subtitulo": "Beatriz & Gabriela",
+        "conteudo": "<p class='w3-justify'>As responsáveis pelo desenvolvimento deste website são <i>Anna Beatriz Faustino Gomes</i> e <i>Gabriela de Freitas Maia</i>, ambas discentes do curso técnico integrado em Informática para Internet do campus São Paulo do Potengi.</p>",
+    },
+
+    {
+        "titulo": "Créditos",
+        "conteudo": "<p class='w3-justify mb-0'>Todas as informações e imagens utilizadas neste site estão disponíveis para uso público na internet e foram utilizadas com fim exclusivamente educacional.</p>",
     }
 ]
 
 context = {
-    "atores": atores,
     "introducao": introducao,
+    "atores": atores,
+    "sobre": sobre,
 }
 
 def index(request):
-    return render(request, 'app/index.html')
+    return render(request, 'app/index.html', context)
 
 def elenco(request):
     return render(request, 'app/elenco.html', context)
 
 def sobre(request):
-    return render(request, 'app/sobre.html')
+    return render(request, 'app/sobre.html', context)
 
 def ator(request, id_ator):
     return render(request, 'app/ator.html', atores[id_ator - 1])
